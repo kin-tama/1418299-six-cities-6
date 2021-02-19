76c8b9c0bfc54196b3a285385d1298d190299ac5
kin-tama <65250935+kin-tama@users.noreply.github.com>
2021-02-19 19:17:38 +0300

    module2-task1

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MainPage from "./main-page";
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
+import MainPage from "./main/main-page";
+import Favorites from "./favorites/favorites";
+import Room from "./room/room";
+import SignIn from "./sign-in/sign-in";
+import NotFound from "./not-found/not-found";
 
 const App = (props) => {
   const {cardsNumber} = props;
-  return <MainPage cardsNumber={cardsNumber} />;
+  return (
+
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage cardsNumber={cardsNumber} />);
+        </Route>
+
+        <Route exact path="/login">
+          <SignIn />);
+        </Route>
+
+        <Route exact path="/favorites">
+          <Favorites />);
+        </Route>
+
+        <Route exact path="/offer/:id">
+          <Room />);
+        </Route>
+
+        <Route path="">
+          <NotFound />);
+        </Route>
+
+      </Switch>
+
+    </BrowserRouter>
+  );
+
 };
 
 App.propTypes = {
---
+++ src/components/not-found/not-found.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <p style={{fontSize: `150px`, fontWeight: `700`}}>
+      <span style={{color: `red`}}>
+        404:
+      </span>
+      <span style={{color: `green`}}>
+        すみません兄！
+      </span>
+    </p>
+  );
+};
+
+export default NotFound;
